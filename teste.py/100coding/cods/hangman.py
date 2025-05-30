2ba5ca24e687579b593970050a604010cca4655e
Heitor Araujo <heitiroliveira925@gmail.com>
2025-05-30 00:20:07 -0400

    Add files via upload

--- teste.py/100coding/cods/hangman.py
+++ teste.py/100coding/cods/hangman.py
@@ -1,14 +1,76 @@
 from random import randint; cw = randint(0, 3)
-list_words = ['camelo', 'paralelo', 'celular', 'thays']
-chosed_word = list_words[cw]
-print(chosed_word)
-lifes = 7
-true_player = []
-while lifes > 0:
-    chosed_player = (input('Guess a letter:')).lower()
-    true_player.append(chosed_player)
-    for i in chosed_word:
-        if i in true_player:
-            print(True)
+x = str('\ ')
+corpo = [
+(f'''
+      +======+
+      0      |
+    / | {x}   |
+    /   {x}   |
+             | '''),
+(f'''
+      +======+
+      0      |
+    / | {x}   |
+    /        |
+             | '''),
+(f'''
+      +======+
+      0      |
+    / |      |
+    /        |
+             | '''),
+(f'''
+      +======+
+      0      |
+    / |      |
+             |
+             | '''),
+(f'''
+      +======+
+      0      |
+      |      |
+             |
+             | '''),
+(f'''
+      +======+
+      0      |
+             |
+             |
+             | '''),
+(f'''
+      +======+
+             |
+             |
+             |
+             | ''')
+]
+lista_palavras = ['camelo', 'paralelo', 'celular', 'thays']
+palavra_escolhida = lista_palavras[cw]
+tamanho = len(palavra_escolhida)
+segredo = ''
+lista = []
+vida = 6
+for para in range(tamanho):
+    segredo += '*'
+print(segredo)
+while True:
+    letra_do_player = str(input('Guess a letter:')).lower()
+    display = ''
+    for letra in palavra_escolhida:
+        if letra == letra_do_player:
+            display += letra
+            lista.append(letra)
+        elif letra in lista:
+            display += letra
         else:
-            print(False)
+            display += '*'
+    if letra_do_player not in palavra_escolhida:
+        vida -= 1
+    print(corpo[vida])
+    print(display)  
+    if '*' not in display:
+        print('You win')
+        break
+    elif vida == 0:
+        print(corpo[vida], 'You lose')
+        break
