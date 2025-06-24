d57dcc3333c81e75b86d7bf3d958b08dae6edcaf
Heitor Araujo <heitiroliveira925@gmail.com>
2025-06-24 00:41:18 -0400

    Update main.py

--- Progresso/CelularParaContatos/main.py
+++ Progresso/CelularParaContatos/main.py
@@ -1,29 +1,42 @@
-from back import Contatos, Menu 
-# Como nossas classe estão em outro arquivo nos importamos elas
-continua = True
-# Aquela condição basica para o laço while funfar
+from back import Contatos, Menu
+from time import sleep
 print('Primeiramente vamos adicionar um numero.')
 fone = Contatos()
-# Usamos a classe contatos logo de inicio porque temos que adicionar pelo menos 1 contato para fazermos qualquer coisa
-while continua:
-    Menu() # Chama o menu como foi explicado
-    x = int(input('Digite um numero: ')) 
-    # recebe um numero inteiro 
-    # E de acordo com oque o usar digitar usando o menu de referencia 
+while True:
+    Menu()
+    x = int(input('Digite um numero: '))
     if x == 1:
-        fone = Contatos() # adiciona mais 1 contato
+        fone = Contatos()
     elif x == 2:
-        print('\n' * 20)
-        fone.ler() # lê todos os contatos
+        print('\n' * 10)
+        fone.ler()
+        sleep(5)
     elif x == 3:
-        print('\n' * 20)
-        fone.pesquisa() # Chama o metodo de pesquisar
+        print('\n' * 10)
+        fone.pesquisa()
+        sleep(5)
     elif x == 4:
-        print('\n' * 20)
-        fone.kamikaze() # apaga a lista de contato
+        print('\n' * 10)
+        fone.kamikaze()
+        sleep(5)
     elif x == 5:
-        fone.deletar() # apaga contato especifico 
-    elif x == 6: # Acabou né
-        break 
-    else: # Caso user não saiba ler ou tem dedos gordos
+        print('\n' * 10)
+        fone.deletar()
+        sleep(5)
+    elif x == 6:
+        print('\n' * 10)
+        fone.backup()
+        sleep(5)
+    elif x == 7:
+        print('\n' * 10)
+        fone.SeeBackup()
+        sleep(5)
+    elif x == 8:
+        print('\n' * 10)
+        sleep(5)
+        print('Finalizando o programa...')
+        sleep
+        break
+    else:
+        print('\n' * 10)
         print('Seleção invalida! Tente novamente.')
