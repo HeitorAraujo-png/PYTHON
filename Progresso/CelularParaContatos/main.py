a9780bff7a6a16643ecf2c973f4d37ad9c9f7de7
Heitor Araujo <heitiroliveira925@gmail.com>
2025-06-29 15:11:56 -0400

    Update main.py

--- Progresso/CelularParaContatos/main.py
+++ Progresso/CelularParaContatos/main.py
@@ -11,80 +11,120 @@ if acesso:
             x = int(input('Digite um numero: '))
         except ValueError:
             x = int(input('Seleção invalida! Tente novamente.\nDigite um NUMERO: '))
-        if x == 1:
-            fone = Contatos(acesso=acesso, senha=senha, user=user)
-        elif x == 2:
-            print('\n' * 10)
-            fone.Ler()
-            sleep(2)
-        elif x == 3:
-            print('\n' * 10)
-            fone.Pesquisa()
-            sleep(2)
-        elif x == 4:
-            print('\n' * 10)
-            fone.Kamikaze(adm=admin)
-            sleep(2)
-        elif x == 5:
-            print('\n' * 10)
-            fone.Deletar()
-            sleep(2)
-        elif x == 6:
-            print('\n' * 10)
-            fone.Backup()
-            sleep(2)
-        elif x == 7:
-            print('\n' * 10)
-            fone.SeeBackup()
-            sleep(2)
-        elif x == 8:
-            print('\n' * 10)
-            print('Finalizando o programa...')
-            sleep(2)
-            break
         if admin:
-            if x == 9:
-                login.AcessoAdm()
-                sleep(1)
-            elif x == 10:
-                login.TrocaSenha()
-                sleep(1) 
-            elif x == 11:
-                login.TrocaNome()
-                sleep(1)
-            elif x == 12:
-                login.DeletaUser()
-                sleep(1)
-            elif x == 13:
-                login.DeletaAdm()
-                sleep(1)                
-            elif x == 14:
-                login.CriaVariosUser()
-                sleep(1)   
-            elif x == 15:
-                login.CriaVariosAdmin()
-                sleep(1)
-            elif x == 16:
-                login.TabelaUserAdmin()
-                sleep(1)
-            elif x == 17:
-                login.TabelaUser()
-                sleep(1)
-            elif x == 18:
-                login.TabelaAdmin()
-                sleep(1)
-            elif x == 19:
-                login.TabelaSecreta()
-                sleep(1)
-            elif x == 20:
-                login.DeletarLinha()
-                sleep(1)
-            else:
+            if x == 1:
+                fone = Contatos(acesso=acesso, senha=senha, user=user)
+            elif x == 2:
                 print('\n' * 10)
-                print('Seleção invalida! Tente novamente.')
-        else:
-            print('\n' * 10)
-            print('Seleção invalida! Tente novamente.')
+                fone.Ler()
+                sleep(2)
+            elif x == 3:
+                print('\n' * 10)
+                fone.Pesquisa()
+                sleep(2)
+            elif x == 4:
+                print('\n' * 10)
+                fone.Kamikaze(adm=admin)
+                sleep(2)
+            elif x == 5:
+                print('\n' * 10)
+                fone.Deletar()
+                sleep(2)
+            elif x == 6:
+                print('\n' * 10)
+                fone.Backup()
+                sleep(2)
+            elif x == 7:
+                print('\n' * 10)
+                fone.SeeBackup()
+                sleep(2)
+            elif x == 8:
+                print(('\n' * 5),'Finalizando o programa...')
+                sleep(2)
+                break
+            elif x == 9:
+                print('\n' * 5)
+                login.AcessoAdm()
+                sleep(2)
+            elif x == 10:
+                print('\n' * 5)
+                login.TrocaSenha()
+                sleep(2) 
+            elif x == 11:
+                print('\n' * 5)
+                login.TrocaNome()
+                sleep(2)
+            elif x == 12:
+                print('\n' * 5)
+                login.DeletaUser()
+                sleep(2)
+            elif x == 13:
+                print('\n' * 5)
+                login.DeletaAdm()
+                sleep(2)                
+            elif x == 14:
+                print('\n' * 5)
+                login.CriaVariosUser()
+                sleep(2)   
+            elif x == 15:
+                print('\n' * 5)
+                login.CriaVariosAdmin()
+                sleep(2)
+            elif x == 16:
+                print('\n' * 5)
+                login.TabelaUserAdmin()
+                sleep(2)
+            elif x == 17:
+                print('\n' * 5)
+                login.TabelaUser()
+                sleep(2)
+            elif x == 18:
+                print('\n' * 5)
+                login.TabelaAdmin()
+                sleep(2)
+            elif x == 19:
+                print('\n' * 5)
+                login.TabelaSecreta()
+                sleep(2)
+            elif x == 20:
+                print('\n' * 5)
+                login.DeletarLinha()
+                sleep(2)
+            else:
+                print(('\n' * 10),'Seleção invalida! Tente novamente.')
+        elif not admin:
+            if x == 1:
+                fone = Contatos(acesso=acesso, senha=senha, user=user)
+            elif x == 2:
+                print('\n' * 10)
+                fone.Ler()
+                sleep(2)
+            elif x == 3:
+                print('\n' * 10)
+                fone.Pesquisa()
+                sleep(2)
+            elif x == 4:
+                print('\n' * 10)
+                fone.Kamikaze(adm=admin)
+                sleep(2)
+            elif x == 5:
+                print('\n' * 10)
+                fone.Deletar()
+                sleep(2)
+            elif x == 6:
+                print('\n' * 10)
+                fone.Backup()
+                sleep(2)
+            elif x == 7:
+                print('\n' * 10)
+                fone.SeeBackup()
+                sleep(2)
+            elif x == 8:
+                print(('\n' * 5),'Finalizando o programa...')
+                sleep(2)
+                break
+            else:
+                print(('\n' * 10),'Seleção invalida! Tente novamente.')
 else:
     print('User cancelou a operação...')
     sleep(2)
