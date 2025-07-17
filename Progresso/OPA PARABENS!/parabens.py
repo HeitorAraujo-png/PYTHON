870792d5cc1b031d2ec907d5b1e5327bfd034355
Heitor <heitiroliveira925@gmail.com>
2025-07-16 20:23:59 -0400

    Arrumando path da musica

--- Progresso/OPA PARABENS!/parabens.py
+++ Progresso/OPA PARABENS!/parabens.py
@@ -8,7 +8,7 @@ if len(dia) < 2:
     dia = f'0{dia}'
 if dia == (datetime.today()).strftime(format="%d") and mes == (datetime.today()).strftime(format="%m"):
     pygame.mixer.init()
-    pygame.mixer.music.load(r'manoel-gomes-parabens.mp3')
+    pygame.mixer.music.load(r'PYTHON\Progresso\OPA PARABENS!\manoel-gomes-parabens.mp3')
     pygame.mixer.music.play()
     print('PARABENS!!!')
     while pygame.mixer.music.get_busy():
