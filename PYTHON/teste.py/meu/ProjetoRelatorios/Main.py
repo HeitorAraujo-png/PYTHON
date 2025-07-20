dc26ec2d5a14a474d60ccba738f21476647fe021
Heitor <heitiroliveira925@gmail.com>
2025-07-19 20:30:21 -0400

    Primeiros passos para fazer projeto de relatorios mensais(PRM) dar certo

--- PYTHON/teste.py/meu/ProjetoRelatorios/Main.py
+++ PYTHON/teste.py/meu/ProjetoRelatorios/Main.py
@@ -1,5 +1,5 @@
 import pandas as pd
 relatorio = pd.read_csv(r'PYTHON\teste.py\meu\ProjetoRelatorios\TESTEPD.csv', encoding='latin1')
-cc = relatorio['C.C'] == 5000
-dt = relatorio.DATA == '31/01/2025'
-print(relatorio.any(dt and cc))
+dt = relatorio[relatorio.DATA == '31/01/2025']
+dtcc = dt[relatorio['C.C'] == 8115]
+print(len(dtcc))
