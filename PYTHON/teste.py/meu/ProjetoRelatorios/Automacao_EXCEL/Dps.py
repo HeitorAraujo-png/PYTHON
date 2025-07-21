b3e255e8971afc0237b0e3fad57128d575fd5418
Heitor <heitiroliveira925@gmail.com>
2025-07-21 00:35:56 -0400

    Tentativa de fazer o projeto ir para frente

--- PYTHON/teste.py/meu/ProjetoRelatorios/Automacao_EXCEL/Dps.py
+++ PYTHON/teste.py/meu/ProjetoRelatorios/Automacao_EXCEL/Dps.py
@@ -3,31 +3,44 @@ import pandas as pd
 class Relatorios:
     
     def __init__(self):
+        self.departamento = pd.read_csv(r'CSV_ARQ\Dptdia.csv', encoding='latin1')
+        df = self.departamento.copy()
+        self.relatorio = df.groupby(['CC', 'DATA']).size().unstack(fill_value=0)
+        self.conq = pd.read_csv(r'CSV_ARQ\relatorioConq.csv', encoding='latin1')
+        self.relatorio.to_csv(r'CSV_ARQ\relatorio.csv', encoding='latin1')
+        
+    def Converte(self):
+        self.relatorio = pd.concat(self.conq.values(), ignore_index=True)
+        print(self.relatorio.head())
+                
+                
+class RelatorioTeste:
+    
+    def __init__(self):
         self.listaCC = [] 
         self.listaDIAS= []
-        self.departamento = pd.read_csv(r'BACKEND_\CSV_ARQ\Dptdia.csv', encoding='latin1')
-        self.relatorio = pd.read_csv(r'BACKEND_\CSV_ARQ\relatorio.csv', encoding='latin1')
+        self.departamento = pd.read_csv(r'CSV_ARQ\Dptdia.csv', encoding='latin1')
+        self.relatorio = pd.read_csv(r'CSV_ARQ\relatorio.csv', encoding='latin1')
         self.CentroCustos()
         self.Dias()
-    
+        
     def remove(self, lista):
         return list(dict.fromkeys(lista))
         
-    def CentroCustos(self):
-        for i in self.departamento.CC:
-            self.listaCC.append(i)
-        self.listaCC.sort()
-        return self.remove(self.listaCC)
-    
     def Dias(self):
         for i in self.departamento.DATA:
             self.listaDIAS.append(i)
         self.listaDIAS.sort()
         return self.remove(self.listaDIAS)
     
+    def CentroCustos(self):
+        for i in self.departamento.CC:
+            self.listaCC.append(i)
+        self.listaCC.sort()
+        return self.remove(self.listaCC)
         
     def ok(self, cc, dia):
-        dpdt = self.departamento[self.departamento['DATA'] == dia]
+        dpdt = self.departamento[self.departamento.DATA == dia]
         dias = dpdt[dpdt.CC == cc]
         print(f'Centro de custo:{cc} | Quantia: {len(dias)} | Data: {dia}')
         
--- PYTHON/teste.py/meu/ProjetoRelatorios/Automacao_EXCEL/main.py
+++ PYTHON/teste.py/meu/ProjetoRelatorios/Automacao_EXCEL/main.py
@@ -1,5 +1,12 @@
 import pandas as pd
 from Dps import *
 
-lista = Relatorios()
-lista.atualiza_relatorio()
+relatorio = Relatorios()
+relatorio.Converte()
+'''Teste'''
+# dias = relatorio.Dias()
+# cc =relatorio.CentroCustos()
+# print(cc, dias)
+# for item in dias:
+#     for i in cc:
+#         relatorio.ok(i, item)
